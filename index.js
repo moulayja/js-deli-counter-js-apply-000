9fa489f4b64ab1de0a58fccb3ea363b34242fb7d
Jamal Farah <moulayjam@gmail.com>
2019-05-28 19:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const takeANumber = (katzDeliLine, name) => {
 }
 
 
-// // 2) Build a function nowServing. This function should accept the current line of people (katzDeliLine) and return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
+ // 2) Build a function nowServing. This function should accept the current line of people (katzDeliLine) and return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
 
 
 const nowServing = katzDeliLine => {
@@ -27,3 +27,21 @@ const nowServing = katzDeliLine => {
     return `Currently serving ${first}.`;
   }
 };
+
+
+// 3)Build a function currentLine that accepts the current line of people and returns the current line as a string; for example, if 'katzDeliLine' is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". You don't have to use katzDeliLine as a variable or parameter name in your function though, it's just an example of a variable that might be passed to it. If there is nobody in line, it should return "The line is currently empty."
+
+const currentLine = () => {
+  var i = 0;
+  while (i < katzDeliLine.length) {
+    inLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
+    i++;
+  }
+  if (katzDeliLine.length > 0) {
+    return (`The line is currently:` + inLine);
+  }else{
+    return `The line is currently empty.`
+  }
+
+
+}
